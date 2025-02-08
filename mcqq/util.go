ebb5ca4849a3f5b38016fcbbac23f29200cbb43d
17TheWord <17theword@gmail.com>
2025-02-08 22:51:32 +0800

    feat(util): 添加 WebSocket 连接清理函数
    - 新增 cleanupWebSocketConnection 函数用于关闭和清理 WebSocket 连接
    - 在 util.go 中导入 websocket 和 logrus 模块
    - 优化了 Minecraft服务器连接断开时的处理逻辑

--- mcqq/util.go
+++ mcqq/util.go
@@ -1,5 +1,10 @@
 package mcqq
 
+import (
+	"github.com/RomiChan/websocket"
+	log "github.com/sirupsen/logrus"
+)
+
 func getTargetServer(serverName string) *MinecraftBot {
 	if server, exist := McBots[serverName]; exist {
 		return server
@@ -25,4 +30,13 @@ func contains(slice []int64, item int64) bool {
 		}
 	}
 	return false
+}
+func cleanupWebSocketConnection(conn *websocket.Conn, serverName string) {
+	err := conn.Close()
+	if err != nil {
+		log.Infof("Close websocket connection from [%s] failed: %v", serverName, err)
+		return
+	}
+	delete(McBots, serverName)
+	log.Infoln("Disconnected from websocket [%s]\n", serverName)
 }
