b589efbf9c5bec655c0983cb8074de965b2ec026
17TheWord <17theword@gmail.com>
2025-02-08 22:50:51 +0800

    refactor(mcqq): 优化日志输出级别和 websocket 连接流程
    - 将日志输出级别从 Warn 改为 Warning，提供更清晰的警告信息
    - 在初始化时添加了 CommandPriority 的信息输出，便于调试
    - 修改了 websocket 连接方式，先启动服务器再启动客户端，提高连接稳定性

--- mcqq/mcqq.go
+++ mcqq/mcqq.go
@@ -10,16 +10,20 @@ var PluginConfig = Config{}
 func init() {
 	if PluginConfig.CommandPriority < 1 {
 		PluginConfig.CommandPriority = 1
-		log.Warn("CommandPriority is too low, set to 1")
+		log.Warning("CommandPriority is too low, set to 1")
 	} else if PluginConfig.CommandPriority > 98 {
 		PluginConfig.CommandPriority = 98
-		log.Warn("CommandPriority is too high, set to 98")
+		log.Warning("CommandPriority is too high, set to 98")
+	} else {
+		log.Infof("CommandPriority is %d, the MessagePriority is %d", PluginConfig.CommandPriority, PluginConfig.CommandPriority+1)
 	}
 
 	if PluginConfig.AccessToken == "" {
-		log.Infoln("AccessToken is empty!!!")
+		log.Warning("AccessToken is empty!!!")
 	}
 
-	StartWebsocket()
+	go startWebsocketServer()
+	startWebsocketClient()
+
 	zero.OnMessage(GroupRule).SetBlock(false).SetPriority(PluginConfig.CommandPriority + 1).Handle(handleQQMessage)
 }
